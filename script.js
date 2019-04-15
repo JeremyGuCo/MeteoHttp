0e3c9db3539526a4aaf5003917c6a4c51c848834
JeremyGueCo <jeremy.guerincoquereau@gmail.com>
2019-04-15 17:53:37 +0200

    MAJ

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-const buttonSubmit = document.getElementById('submit')
-const temp = document.getElementById('testTemp')
-const search = document.getElementById('citySearch');
-
-
-buttonSubmit.addEventListener('click', function () {
-   let city = search.value;
+document.getElementById('submit').addEventListener('click', function () {
+   let city = document.getElementById('citySearch').value;
    let api = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=1ee6993c8162e52ac6d12a36b88f9478`
 
    fetch(api)
